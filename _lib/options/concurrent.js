9695090a8103ac9faac8125f88150652c400fa84
Hannes Diercks <hi@xiphe.net>
2015-06-01 17:34:44 +0200

    chore: remove lib backup

--- _lib/options/concurrent.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  demo: ['watch:demo', 'shell:openDemo'],
-  options: {
-    logConcurrentOutput: true
-  }
-};
--- _lib/templates/wrapTop.tpl
+++
@@ -1,3 +0,0 @@
-(function(jQuery, undefined) {
-  'use strict';
-
